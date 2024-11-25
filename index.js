c25a82c587c3ee38cde73144b49f4e764046f82a
Taketakemagic <168814866+Taketakemagic@users.noreply.github.com>
2024-11-25 21:26:20 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function check1(index){
+  var message1 = document.getElementById("message1");
+  if(index == 1){
+    document.getElementById("message1").textContent = "今日は終了・明日の自分に任せろ";
+  }
+  else if(index == 3){
+    document.getElementById("message2").textContent = "今日は終了・絶対負ける";
+  }
+  else if(index == 5){
+    document.getElementById("message3").textContent = "1度PCを落として1から2時間トレードから離れる。再度確認この項目を確認";
+  }
+  else if(index == 7){
+    document.getElementById("message4").textContent = "次のエントリーで巻き返そうとせずそういう時こそ落ち着いてエントリーしろ";
+  }
+}
+function check2(index){
+  const step = document.getElementById('step'+index);
+  step.classList.remove('hidden');
+}
