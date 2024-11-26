2c1555096ae39b71e65d3e5d1ef721fb4ba355db
Taketakemagic <tandritikin@gmail.com>
2024-11-26 20:47:01 +0900

    upd1
    メッセージの更新

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function check1(index){
     document.getElementById("message1").textContent = "今日は終了・明日の自分に任せろ";
   }
   else if(index == 3){
-    document.getElementById("message2").textContent = "今日は終了・絶対負ける";
+    document.getElementById("message2").textContent = "角度の固定・クロスの前兆とストキャスティクスの角度は両立させろ";
   }
   else if(index == 5){
     document.getElementById("message3").textContent = "1度PCを落として1から2時間トレードから離れる。再度確認この項目を確認";
